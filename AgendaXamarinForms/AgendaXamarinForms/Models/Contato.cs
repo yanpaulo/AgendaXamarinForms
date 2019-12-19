a7e413f511a6eaa960f5ae48016c0293416db5bc
Yan Paulo <yanpaulo@hotmail.com>
2019-12-19 16:36:11 -0300

    Corrige um bug que impedia contatos múltiplos contatos de serem salvos

--- AgendaXamarinForms/AgendaXamarinForms/Models/Contato.cs
+++ AgendaXamarinForms/AgendaXamarinForms/Models/Contato.cs
@@ -8,7 +8,7 @@ namespace AgendaXamarinForms.Models
     public class Contato
     {
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         public string Nome { get; set; }
 
